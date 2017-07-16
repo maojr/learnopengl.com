c52fe37a8cd198fbc163a3e03420868c57fbb49e
Love Cocos <maojr@github.com>
2017-07-16 16:34:29 +0800

    source code for learnopengl.com

---
+++ exercises/camera/solution2/camera.h
@@ -0,0 +1,155 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <vector>
+
+enum Camera_Movement {
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+// Default camera values
+const float YAW        = -90.0f;
+const float PITCH      = 0.0f;
+const float SPEED      = 2.5f;
+const float SENSITIVTY = 0.1f;
+const float ZOOM       = 45.0f;
+
+// An abstract camera class that processes input and calculates the corresponding Eular Angles, Vectors and Matrices for use in OpenGL
+class Camera
+{
+public:
+	// Camera Attributes
+	glm::vec3 Position;
+	glm::vec3 Front;
+	glm::vec3 Up;
+	glm::vec3 Right;
+	glm::vec3 WorldUp;
+	// Eular Angles
+	float Yaw;
+	float Pitch;
+	// Camera options
+	float MovementSpeed;
+	float MouseSensitivity;
+	float Zoom;
+
+	// Constructor with vectors
+	Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVTY), Zoom(ZOOM)
+	{
+		Position = position;
+		WorldUp = up;
+		Yaw = yaw;
+		Pitch = pitch;
+		updateCameraVectors();
+	}
+	// Constructor with scalar values
+	Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVTY), Zoom(ZOOM)
+	{
+		Position = glm::vec3(posX, posY, posZ);
+		WorldUp = glm::vec3(upX, upY, upZ);
+		Yaw = yaw;
+		Pitch = pitch;
+		updateCameraVectors();
+	}
+	// Returns the view matrix calculated using Eular Angles and the LookAt Matrix
+	glm::mat4 GetViewMatrix()
+	{
+		// return glm::lookAt(Position, Position + Front, Up);
+		return calculate_lookAt_matrix(Position, Position + Front, Up);
+	}
+	// Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM(to abstrace it from windowing systems)
+	void ProcessKeyboard(Camera_Movement direction, float deltaTime)
+	{
+		float velocity = MovementSpeed * deltaTime;
+		if(direction == FORWARD)
+			Position += Front * velocity;
+		if(direction == BACKWARD)
+			Position -= Front * velocity;
+		if(direction == LEFT)
+			Position -= Right * velocity;
+		if(direction == RIGHT)
+			Position += Right * velocity;
+	}
+	// Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
+	void ProcessMouseMovement(float xoffset, float yoffset, bool constrainPitch = true)
+	{
+		xoffset *= MouseSensitivity;
+		yoffset *= MouseSensitivity;
+
+        Yaw   += xoffset;
+        Pitch += yoffset;
+
+        // Make sure that when pitch is out of bounds, screen doesn't get flipped
+        if (constrainPitch)
+        {
+            if (Pitch > 89.0f)
+                Pitch = 89.0f;
+            if (Pitch < -89.0f)
+                Pitch = -89.0f;
+        }
+
+        // Update Front, Right and Up Vectors using the updated Eular angles
+        updateCameraVectors();
+	}
+	// Processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+	void ProcessMouseScroll(float yoffset)
+	{
+		if (Zoom >= 1.0f && Zoom <= 45.0f)
+			Zoom -= yoffset;
+		if (Zoom <= 1.0f)
+			Zoom = 1.0f;
+		if (Zoom >= 45.0f)
+			Zoom = 45.0f;
+	}
+private:
+	// Custom implementation of the LookAt function
+	glm::mat4 calculate_lookAt_matrix(glm::vec3 position, glm::vec3 target, glm::vec3 worldUp)
+	{
+	    // 1. Position = known
+	    // 2. Calculate cameraDirection
+	    glm::vec3 zaxis = glm::normalize(position - target);
+	    // 3. Get positive right axis vector
+	    glm::vec3 xaxis = glm::normalize(glm::cross(glm::normalize(worldUp), zaxis));
+	    // 4. Calculate camera up vector
+	    glm::vec3 yaxis = glm::cross(zaxis, xaxis);
+
+	    // Create translation and rotation matrix
+	    // In glm we access elements as mat[col][row] due to column-major layout
+	    glm::mat4 translation; // Identity matrix by default
+	    translation[3][0] = -position.x; // Third column, first row
+	    translation[3][1] = -position.y;
+	    translation[3][2] = -position.z;
+	    glm::mat4 rotation;
+	    rotation[0][0] = xaxis.x; // First column, first row
+	    rotation[1][0] = xaxis.y;
+	    rotation[2][0] = xaxis.z;
+	    rotation[0][1] = yaxis.x; // First column, second row
+	    rotation[1][1] = yaxis.y;
+	    rotation[2][1] = yaxis.z;
+	    rotation[0][2] = zaxis.x; // First column, third row
+	    rotation[1][2] = zaxis.y;
+	    rotation[2][2] = zaxis.z; 
+
+	    // Return lookAt matrix as combination of translation and rotation matrix
+	    return rotation * translation; // Remember to read from right to left (first translation then rotation)
+	}
+	// Calculates the fron vector from the Camera's (updated) Eular Angles
+	void updateCameraVectors()
+	{
+		// Calculate the new Front vector
+		glm::vec3 front;
+		front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+		front.y = sin(glm::radians(Pitch));
+		front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+		Front = glm::normalize(front);
+		// Also re-calculate the Right and Up vector
+		Right = glm::normalize(glm::cross(Front, WorldUp)); // Normalize the vectors, because their length gets closer to 0 the more you lookup or down which results in slower movement.
+		Up    = glm::normalize(glm::cross(Right, Front));
+	}
+};
+#endif
---
+++ window/HelloWindow.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <glfw/glfw3.h>
+
+// Function prototypes
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+    	std::cout << "Failed to create GLFW window" << std::endl;
+    	glfwTerminate();
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+    	std::cout << "Failed to initialize GLEW" << std::endl;
+    	return -1;
+    }
+
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    while(!glfwWindowShouldClose(window))
+    {
+    	glfwPollEvents();
+    	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    	glClear(GL_COLOR_BUFFER_BIT);
+    	glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	std::cout << key << std::endl;
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
